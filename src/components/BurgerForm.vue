5241904b61c4f01b7968267e0b30fcfddcbfcdbe
Eduardo <eduardoatk@gmail.com>
2022-03-20 23:05:49 -0300

    Tentando integrar back, mas está dando errado

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,119 +1,175 @@
 <template>
-    <div>
-        <p>Componente de Mensagem</p>
-        <div>
-            <form id="burger-form">
-                <div class="input-container">
-                    <label for="nome">Nome do Cliente:</label>
-                    <input
-                        type="text"
-                        id="nome"
-                        name="nome"
-                        v-model="nome"
-                        placeholder="Digite o seu nome"
-                    />
-                </div>
-                <div class="input-container">
-                    <label for="pao">Escolha o pão:</label>
-                    <select name="pao" id="pao" v-model="pao">
-                        <option value>Selecione o seu pão</option>
-                        <option value="integral">Integral</option>
-                    </select>
-                </div>
-                <div class="input-container">
-                    <label for="carne">Escolha a carne do seu Burguer:</label>
-                    <select name="carne" id="carne" v-model="carne">
-                        <option value>Selecione o tipo de carne</option>
-                        <option value="maminha">Maminha</option>
-                    </select>
-                </div>
-                <div id="opcionais-container" class="input-container">
-                    <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-                    <div class="checkbox-container">
-                        <input type="checkbox" name="opcionais" v-model="opcionais" value="salame" />
-                        <span>Salame</span>
-                    </div>
-                </div>
-                <div class="input-container">
-                    <input type="submit" class="submit-btn" value="Criar meu Burger" />
-                </div>
-            </form>
-        </div>
-    </div>
+   <div>
+      <p>Componente de Mensagem</p>
+      <div>
+         <form id="burger-form" @submit="createBurger">
+            <div class="input-container">
+               <label for="nome">Nome do Cliente:</label>
+               <input
+                  type="text"
+                  id="nome"
+                  name="nome"
+                  v-model="nome"
+                  placeholder="Digite o seu nome"
+               />
+            </div>
+            <div class="input-container">
+               <label for="pao">Escolha o pão:</label>
+               <select name="pao" id="pao" v-model="pao">
+                  <option value>Selecione o seu pão</option>
+                  <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
+               </select>
+            </div>
+            <div class="input-container">
+               <label for="carne">Escolha a carne do seu Burguer:</label>
+               <select name="carne" id="carne" v-model="carne">
+                  <option value>Selecione o tipo de carne</option>
+                  <option
+                     v-for="carne in carnes"
+                     :key="carne.id"
+                     :value="carne.tipo"
+                  >{{ carne.tipo }}</option>
+               </select>
+            </div>
+            <div id="opcionais-container" class="input-container">
+               <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+               <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                  <input
+                     type="checkbox"
+                     name="opcionais"
+                     v-model="opcionais"
+                     :value="opcional.tipo"
+                  />
+                  <span>{{ opcional.tipo }}</span>
+               </div>
+            </div>
+            <div class="input-container">
+               <input type="submit" class="submit-btn" value="Criar meu Burger" />
+            </div>
+         </form>
+      </div>
+   </div>
 </template>
 
-<script>
+<script>import { onMounted } from "vue";
+
 export default {
-    name: "BurgerForm",
+   name: "BurgerForm",
+   data() {
+      return {
+         paes: null,
+         carnes: null,
+         opcionaisdata: null,
+         nome: null,
+         pao: null,
+         carne: null,
+         opcionais: [],
+         status: "Solicitado",
+         msg: null
+      }
+   },
+   methods: {
+      async getIngredientes() {
+
+         const req = await fetch("http://192.168.1.112:3000/ingredientes")
+         const data = await req.json()
+
+         this.paes = data.paes
+         this.carnes = data.carnes
+         this.opcionaisdata = data.opcionais
+
+      },
+      async createBruger(e) {
+         e.preventDefault()
+         const data = {
+            nome: this.nome,
+            pao: this.pao,
+            carne: this.carne,
+            opcionais: Array.from(this.opcionais),
+            status: "Solicitado"
+         }
+         const dataJson = JSON.stringify(data)
+         const req = await fetch("http://192.168.1.112:3000/burgers", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: dataJson
+         })
+         const res = await req.json()
+         console.log(res)
+      }
+   },
+   mounted() {
+      this.getIngredientes()
+   }
 }
 </script>
 
 <style scoped>
-    #burger-form {
-        max-width: 400px;
-        margin: 0 auto;
-    }
-    
-    .input-container {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-    }
+#burger-form {
+   max-width: 400px;
+   margin: 0 auto;
+}
 
-    label {
-        font-weight: bold;
-        margin-bottom: 15px;
-        color: #222;
-        padding: 5px 10px;
-        border-left: 4px solid #FCBA03;
-    }
+.input-container {
+   display: flex;
+   flex-direction: column;
+   margin-bottom: 20px;
+}
 
-    input, select {
-        padding: 5px 10px;
-        width: 300px;
-    }
+label {
+   font-weight: bold;
+   margin-bottom: 15px;
+   color: #222;
+   padding: 5px 10px;
+   border-left: 4px solid #fcba03;
+}
 
-    #opcionais-container {
-        flex-direction: row;
-        flex-wrap: wrap;
-    }
+input,
+select {
+   padding: 5px 10px;
+   width: 300px;
+}
 
-    #opcionais-title {
-        width: 100%;
-    }
+#opcionais-container {
+   flex-direction: row;
+   flex-wrap: wrap;
+}
 
-    .checkbox-container {
-        display: flex;
-        align-items: flex-start;
-        width: 50%;
-        margin-bottom: 20px;
-    }
+#opcionais-title {
+   width: 100%;
+}
 
-    .checkbox-container span,
-    .checkbox-container input {
-        width: auto;
-        
-    }
+.checkbox-container {
+   display: flex;
+   align-items: flex-start;
+   width: 50%;
+   margin-bottom: 20px;
+}
 
-    .checkbox-container span {
-        margin-left: 6px;
-        font-weight: bold;
-    }
+.checkbox-container span,
+.checkbox-container input {
+   width: auto;
+}
 
-    .submit-btn {
-        background-color: #222;
-        color: #FCBA03;
-        font-weight: bold;
-        border: 2px solid #222;
-        padding: 10px;
-        font-size: 16px;
-        margin: 0 auto;
-        cursor: pointer;
-        transition: .5s;
-    }
+.checkbox-container span {
+   margin-left: 6px;
+   font-weight: bold;
+}
 
-    .submit-btn:hover {
-        background-color: transparent;
-        color: #222;
-    }
+.submit-btn {
+   background-color: #222;
+   color: #fcba03;
+   font-weight: bold;
+   border: 2px solid #222;
+   padding: 10px;
+   font-size: 16px;
+   margin: 0 auto;
+   cursor: pointer;
+   transition: 0.5s;
+}
+
+.submit-btn:hover {
+   background-color: transparent;
+   color: #222;
+}
 </style>
